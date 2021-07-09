77f58c8d870b75e35d2baf48055235cd503fcf23
Gillian Baker <gillianbaker97@gmail.com>
2021-07-08 21:52:25 -0400

    new update

---
+++ script.js
@@ -0,0 +1,29 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var set = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
+var length = 10;
+var newstring = ''
+
+function generatePassword(10, set) {
+  for (var i = 0; i <length; i++) {
+  var floor = Math.floor(Math.random() * (set1.length + set2.length + set3.length));
+  newstring += set1.substring(floor, floor+1);
+}
+  return newstring;
+}
+
+document.getElementById("newstring2") = newstring;
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
